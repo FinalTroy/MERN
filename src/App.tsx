c79d32cbd6a7b93ec28bd844e6954f177a52a4ae
FinalTroy <ondrejicka.tadeas@gmail.com>
2020-04-21 13:01:22 +0200

    tisk

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Navbar from './components/navbar.component'
@@ -10,16 +11,48 @@ import CreateTrainer from './components/Pages/Create-trainer'
 import Generator from './components/Pages/Create-generator';
 
 const App = () => {
+  const [trainerList, setTrainerList] = useState<any[]>([])
+  const [exercises, setExercises] = useState<any[]>([])
+
+  useEffect(() => {
+      axios.get('http://localhost:5000/exercises/')
+          .then(res => {
+              setExercises(res.data)
+          })
+          .catch(err => console.log(err))
+  }, [])
+  useEffect(() => {
+    axios.get('http://localhost:5000/users/')
+      .then(res => {
+        setTrainerList(res.data)
+      })
+      .catch(err => console.log(err))
+  })
   return (
     <Router>
       <div className='container'>
         <Navbar />
         <br />
-        <Route path='/' exact component={ExercisesList} />
-        <Route path='/edit/:id' exact component={EditExercise} />
-        <Route path='/create' exact component={CreateExercise} />
-        <Route path='/user' exact component={CreateTrainer} />
-        <Route path='/generator' exact component={Generator} />
+        <Route exact path='/'>
+          <ExercisesList />
+        </Route>
+        <Route exact path='/edit/:id'>
+          <EditExercise match/>
+        </Route>
+        <Route exact path='/create'>
+          <CreateExercise />
+        </Route>
+        <Route exact path='/user'>
+          <CreateTrainer 
+            trainerList={trainerList}
+          />
+        </Route>
+        <Route exact path='/generator'>
+          <Generator 
+            trainerList={trainerList}
+            exercises={exercises}
+          />
+        </Route>
       </div>
     </Router>
 
