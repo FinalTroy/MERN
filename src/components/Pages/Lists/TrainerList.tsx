d1db3e7f0a5f1f0ea5693f002c774a5fb5605de0
FinalTroy <ondrejicka.tadeas@gmail.com>
2020-04-23 13:38:52 +0200

    nova componenta colorPicker a colorShower

--- src/components/Pages/Lists/TrainerList.tsx
+++ src/components/Pages/Lists/TrainerList.tsx
@@ -1,18 +1,22 @@
 // import { Link } from 'react-router-dom';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ColorShower from '../Custom/ColorShower';
+import { IColor } from '../Create-trainer';
 
 interface IProps {
     trainer: {
         trainer: string;
         _id: string
     }
+    color: IColor;
     deleteTrainer(id: string): void
 }
 const TrainerList = (props: IProps) => {
     return (
         <tr className="row justify-content-center">
             <td className="col-md-auto">{props.trainer.trainer}</td>
+            <ColorShower color={props.color} />
             <td className="col-md-auto">
                 <Link to="#top" onClick={() => { props.deleteTrainer(props.trainer._id) }}>delete</Link>
             </td>
